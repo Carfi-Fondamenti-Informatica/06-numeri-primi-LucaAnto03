2681963b139392e6d40ad67270a50cd2652e13fa
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-12-01 21:00:10 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
+#include <iostream>
 
+using namespace std;
+void primo (int n) {
+    int i = 1, resto=0;
+    do {
+    i++;
+    resto=n%i;
+    } while(resto);
+    if (n==i) {
+        cout << "Vero" << endl;
+        return;
+    } else {
+        cout << "False" << endl;
+        return;
+    }
+}
